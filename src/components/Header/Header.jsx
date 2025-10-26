34faedbf86b5999afc3bb929281276c30a979524
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-26 10:25:45 +0300

    Implement Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,27 @@ import './Header.css'
 export default function Header() {
     return (
         <div>
-            
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-danger fw-bold px-5" href="#">Products Project</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link px-3" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link px-3" href="#">Products</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link px-3 me-5" href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }

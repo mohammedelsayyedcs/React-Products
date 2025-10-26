04635d063b0b8626da1939ee37953f24ea833679
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-26 10:13:55 +0300

    Build Project Skeleton

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NotFound.css'
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Not Found</h1>
+        </div>
+    )
+}
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Products.css'
+
+export default function 
+() {
+  return (
+    <div>
+        <h1>Products</h1>
+    </div>
+  )
+}

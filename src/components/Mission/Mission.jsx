2c76ffea1dce520784195fd6d06c22d10131d547
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-28 17:59:24 +0300

    Implement goBackward and goForward in Mission and Vission respectively

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Mission.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function Mission() {
-  return (
-    <div>
-        <h3>Mission</h3>
-    </div>
-  )
+
+    const navTo = useNavigate()
+    const goBackward = () => {
+        navTo(-1)
+    }
+
+    return (
+        <div>
+            <h3>Mission</h3>
+            <button className='btn btn-info px-4 py-2 text-white mt-2' onClick={goBackward} >Back</button>
+        </div>
+    )
 }
--- src/components/Vision/Vision.jsx
+++ src/components/Vision/Vision.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Vision.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function Vision() {
+    const navTo = useNavigate()
+    const goForward = () => {
+        navTo(1)
+    }
     return (
         <div>
             <h3>Vision</h3>
+            <button className='btn btn-danger px-4 py-2 mt-3' onClick={goForward}>Forward</button>
         </div>
     )
 }

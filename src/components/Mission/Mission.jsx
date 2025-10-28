0f7e1fcbc5dbd514dd3e999b9be2ef9f3a02230a
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-28 17:33:47 +0300

    Create About, Mission, and Vision Components

---
+++ src/components/Mission/Mission.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Mission.css'
+
+export default function Mission() {
+  return (
+    <div>
+        <h3>Mission</h3>
+    </div>
+  )
+}
---
+++ src/components/Vision/Vision.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Vision.css'
+
+export default function Vision() {
+    return (
+        <div>
+            <h3>Vision</h3>
+        </div>
+    )
+}

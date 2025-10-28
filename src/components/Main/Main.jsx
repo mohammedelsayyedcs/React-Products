a4b80a651bcb9ce93aee7dba0f4456c455513250
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-28 18:36:27 +0300

    Fix Loading Images in Products.jsx When Clicking on Main Link Inside Sidebar.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
         cards.push(
 
             <div key={index} className="col-4 card px-3 border-0">
-                <img src={`./${index}.webp`} style={{ width: '300px', height: '200px' }} className="card-img-top" alt="..." />
+                <img src={`/${index}.webp`} style={{ width: '300px', height: '200px' }} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>

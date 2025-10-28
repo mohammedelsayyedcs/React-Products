ee21fc9a44721d3307230a558006be7ab2c706fa
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-28 18:21:41 +0300

    Implement Main.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,22 +3,6 @@ import './Products.css'
 import Sidebar from '../Sidebar/Sidebar'
 
 export default function Products() {
-  const cards = []
-
-  for (let index = 1; index <= 3; index++) {
-    cards.push(
-
-      <div class="col-4 card px-3 border-0">
-        <img src={`./${index}.webp`} style={{ width: '300px', height: '200px' }} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className='body row'>
       <div className='col-2'>

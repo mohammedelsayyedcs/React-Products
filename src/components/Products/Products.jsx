c990fffb512fe50cd4680b64b13d00e3c44d8e98
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-10-30 17:33:00 +0300

    Get All Products and List Them

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Products.css'
+import axios from 'axios';
 
 export default function Products() {
+  // Set Variable to store products
+  const [products, setProducts] = useState([]);
+
+  // Get/Fetch all products
+  const getProducts = async (url) => {
+    try {
+      const res = await axios.get(url);
+      setProducts(res.data)
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  // Call getProducts in page first load
+  useEffect(() => { getProducts('https://fakestoreapi.com/products') }, [])
+
   return (
     <div className='body'>
-      <h1>Products</h1>
+      <ol>
+        {products.map((item) => {
+          return <li key={item.id}>{item.title}</li>
+        })}
+      </ol>
     </div>
   )
 }

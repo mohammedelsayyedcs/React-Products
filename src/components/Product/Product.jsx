d637aef4cc27633d1241e82f4342a22bf5a82ce8
mohammedelsayyedcs <mohammed.elsayyed.cs@gmail.com>
2025-11-16 14:55:06 +0200

    Enhance UI in Products.jsx and ProductDetails.jsx a Little

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -13,10 +13,10 @@ export default function Product(props) {
         <div className='col-sm-12 col-md-6 col-lg-4 p-3 h-25'>
             <div className="card border-0">
                 <img src={props.obj.image} style={{ height: '250px' }} className="card-img-top" alt={props.obj.title} />
-                <div className="card-body">
-                    <h5 className="card-title">{props.obj.title}</h5>
+                <div className="card-body lead">
+                    <h5 className="card-title" style={{ height: "70px" }}>{props.obj.title}</h5>
                     <h3 className='text-danger'>{props.obj.price}$</h3>
-                    <p className="card-text text-truncate">{props.obj.description}</p>
+                    <p className="card-text text-truncate" style={{ height: "50px" }}>{props.obj.description}</p>
                     <a className="btn btn-primary" onClick={() => { viewProductDetails(props.obj.id) }}>View Details</a>
                 </div>
             </div>
--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -30,12 +30,12 @@ export default function ProductDetails() {
 
     return (
         <div className='body row container mx-auto'>
-            <div className='col-sm-12 col-md-6'>
+            <div className='col-sm-12 col-md-6 pe-5'>
                 <img src={product.image} className="card-img-top" alt={product.title} />
             </div>
 
-            <div className="col-sm-12 col-md-6">
-                <h5 className="card-title py-2">{product.title}</h5>
+            <div className="col-sm-12 col-md-6 lead mt-5 ps-5">
+                <h4 className="card-title py-2">{product.title}</h4>
                 <h3 className='text-info py-2'>{product.price}$</h3>
                 <p className="card-text py-2">{product.description}</p>
                 <a className="btn btn-danger px-4 py-2 mt-4" onClick={goBack}>Back</a>
